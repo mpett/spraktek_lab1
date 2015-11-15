67a0aa2a412d21bd44cdc58876f8ae7e078305fc
Martin Pettersson <kontakt@martinpettersson.se>
2015-11-15 19:21:45 +0100

    Deluppgift 3, partial

---
+++ grammar3.pl
@@ -0,0 +1,76 @@
+% Grammatikregler
+s --> np.
+
+np --> det(Genus), jj(Genus), n.
+np --> det(Genus), jj(Genus), n, n.
+
+% Lexikon
+det(utrum) --> [ den ].
+det(neutrum) --> [ det ].
+det(utrum) --> [ nagra ].
+det(neutrum) --> [ nagra ].
+det(utrum) --> [ de ].
+det(neutrum) --> [ de ].
+det(utrum) --> [ en ].
+det(neutrum) --> [ ett ].
+
+jj(utrum) --> [ gammal ].
+jj(neutrum) --> [ gammalt ].
+jj(utrum) --> [ gamle ].
+jj(neutrum) --> [ gamla ].
+
+jj(utrum) --> [ rod ].
+jj(neutrum) --> [ rott ].
+jj(utrum) --> [ rode ].
+jj(neutrum) --> [ roda ].
+
+n --> [ man ].
+n --> [ mannen ].
+n --> [ maan ].
+n --> [ mannen ].
+n --> [ mans ].
+n --> [ mannens ].
+n --> [ maans ].
+n --> [ maannens ].
+
+n --> [ kvinna ].
+n --> [ kvinnan ].
+n --> [ kvinnor ].
+n --> [ kvinnorna ].
+n --> [ kvinnas ].
+n --> [ kvinnans ].
+n --> [ kvinnors ].
+n --> [ kvinnornas ].
+
+n --> [ bord ].
+n --> [ bordet ].
+n --> [ borden ].
+n --> [ bords ].
+n --> [ bordets ].
+n --> [ bordens ].
+
+n --> [ skal ].
+n --> [ skalet ].
+n --> [ skalen ].
+n --> [ skals ].
+n --> [ skalets ].
+n --> [ skalens ].
+
+n --> [ apple].
+n --> [ applet ].
+n --> [ applen ].
+n --> [ applena ].
+n --> [ apples ].
+n --> [ applets ].
+n --> [ applens ].
+n --> [ applenas ].
+
+n --> [ kant ].
+n --> [ kanten ].
+n --> [ kanter ].
+n --> [ kanterna ].
+n --> [ kants ].
+n --> [ kantens ].
+n --> [ kanters ].
+n --> [ kanternas ].
+
