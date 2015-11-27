025928bba7d2a7d2453689b4bc3cfb277d37277e
Martin Pettersson <kontakt@martinpettersson.se>
2015-11-27 02:38:11 +0100

    Added more grammaical rules

--- grammar3.pl
+++ grammar3.pl
@@ -1,84 +1,85 @@
 % Grammatikregler
 s --> np.
 
-np --> det(Genus, Numerus), jj(Genus, Numerus), n(Genus, Numerus).
-np --> det(Genus, Numerus), jj(Genus, Numerus), n(Genus, Numerus), n(Genus, Numerus).
+np --> det(Genus, Numerus, Species), jj(Genus, Numerus, Species), n(Genus, Numerus, Species, Kasus).
+np --> det(Genus, Numerus, Species), jj(Genus, Numerus, Species), n(Genus, Numerus, Species, Kasus), n(Genus, Numerus, Species, Kasus).
 
 % Lexikon
-det(utrum, singularis) --> [ den ].
-det(neutrum, singularis) --> [ den ].
-det(neutrum, singularis) --> [ det ].
-det(utrum, singularis) --> [ det ].
-det(utrum, pluralis) --> [ nagra ].
-det(neutrum, pluralis) --> [ nagra ].
-det(utrum, pluralis) --> [ de ].
-det(neutrum, pluralis) --> [ de ].
-det(utrum, singularis) --> [ en ].
-det(neutrum, singularis) --> [ ett ].
+det(utrum, singularis, definit) --> [ den ].
+det(neutrum, singularis, definit) --> [ det ].
+det(utrum, pluralis, indefinit) --> [ nagra ].
+det(neutrum, pluralis, indefinit) --> [ nagra ].
+det(utrum, pluralis, definit) --> [ de ].
+det(neutrum, pluralis, definit) --> [ de ].
+det(utrum, singularis, indefinit) --> [ en ].
+det(neutrum, singularis, indefinit) --> [ ett ].
 
-jj(utrum, singularis) --> [ gammal ].
-jj(neutrum, singularis) --> [ gammalt ].
-jj(utrum, pluralis) --> [ gamle ].
-jj(neutrum, singularis) --> [ gamle ].
-jj(neutrum,pluralis) --> [ gamla ].
-jj(utrum,pluralis) --> [ gamla ].
-jj(utrum,singularis) --> [ gamla ].
+jj(utrum, singularis, indefinit) --> [ gammal ].
+jj(neutrum, singularis, indefinit) --> [ gammalt ].
+jj(utrum, pluralis, definit) --> [ gamle ].
+jj(utrum, singularis, definit) --> [ gamle ].
+jj(neutrum,pluralis, definit) --> [ gamla ].
+jj(utrum,pluralis, definit) --> [ gamla ].
+jj(neutrum,pluralis, definit) --> [ gamla ].
+jj(utrum,singularis, definit) --> [ gamla ].
+jj(neutrum,singularis, definit) --> [ gamla ].
 
-jj(utrum, singularis) --> [ rod ].
-jj(neutrum, singularis) --> [ rott ].
-jj(utrum, singularis) --> [ rode ].
-jj(neutrum, pluralis) --> [ roda ].
-jj(neutrum, singularis) --> [ roda ].
+jj(utrum, singularis, indefinit) --> [ rod ].
+jj(neutrum, singularis, indefinit) --> [ rott ].
+jj(utrum, singularis, definit) --> [ rode ].
+jj(neutrum, pluralis, definit) --> [ roda ].
+jj(neutrum, singularis, definit) --> [ roda ].
 
+n(utrum, singularis, indefinit, nominativ) --> [ man ].
+n(utrum, singularis, definit, nominativ) --> [ mannen ].
+n(utrum, pluralis, indefinit, nominativ) --> [ maan ].
+n(utrum, singularis, definit, nominativ) --> [ mannen ].
+n(utrum, singularis, indefinit, genitiv) --> [ mans ].
+n(utrum, singularis, definit, genitiv) --> [ mannens ].
+n(utrum, pluralis, indefinit, genitiv) --> [ maans ].
+n(utrum, pluralis, definit, genitiv) --> [ maannens ].
+n(utrum, pluralis, definit, nominativ) --> [ maannen ].
 
-n(neutrum, singularis) --> [ man ].
-n(neutrum, singularis) --> [ mannen ].
-n(neutrum, pluralis) --> [ maan ].
-n(neutrum, singularis) --> [ mannen ].
-n(neutrum, singularis) --> [ mans ].
-n(neutrum, singularis) --> [ mannens ].
-n(neutrum, pluralis) --> [ maans ].
-n(neutrum, pluralis) --> [ maannens ].
-n(neutrum, pluralis) --> [ maannen ].
+n(utrum, singularis, indefinit, nominativ) --> [ kvinna ].
+n(utrum, singularis, definit, nominativ) --> [ kvinnan ].
+n(utrum, pluralis, indefinit, nominativ) --> [ kvinnor ].
+n(utrum, pluralis, definit, nominativ) --> [ kvinnorna ].
+n(utrum, singularis, indefinit, genitiv) --> [ kvinnas ].
+n(utrum, singularis, definit,  genitiv) --> [ kvinnans ].
+n(utrum, pluralis, indefinit, genitiv) --> [ kvinnors ].
+n(utrum, pluralis, definit, genitiv) --> [ kvinnornas ].
 
-n(utrum, singularis) --> [ kvinna ].
-n(utrum, singularis) --> [ kvinnan ].
-n(utrum, pluralis) --> [ kvinnor ].
-n(utrum, pluralis) --> [ kvinnorna ].
-n(utrum, singularis) --> [ kvinnas ].
-n(utrum, singularis) --> [ kvinnans ].
-n(utrum, pluralis) --> [ kvinnors ].
-n(utrum, pluralis) --> [ kvinnornas ].
+n(neutrum, singularis, indefinit, nominativ) --> [ bord ].
+n(neutrum, pluralis, indefinit, nominativ) --> [ bord ].
+n(neutrum, singularis, definit, nominativ) --> [ bordet ].
+n(neutrum, pluralis, definit, nominativ) --> [ borden ].
+n(neutrum, singularis, indefinit, genitiv) --> [ bords ].
+n(neutrum, pluralis, indefinit, genitiv) --> [ bords ].
+n(neutrum, singularis, definit, genitiv) --> [ bordets ].
+n(neutrum, pluralis, definit, genitiv) --> [ bordens ].
 
-n(neutrum, singularis) --> [ bord ].
-n(neutrum, singularis) --> [ bordet ].
-n(neutrum, pluralis) --> [ borden ].
-n(neutrum, singularis) --> [ bords ].
-n(neutrum, singularis) --> [ bordets ].
-n(neutrum, pluralis) --> [ bordens ].
+n(neutrum, singularis, indefinit, nominativ) --> [ skal ].
+n(neutrum, singularis, definit, nominativ) --> [ skalet ].
+n(neutrum, pluralis, definit, nominativ) --> [ skalen ].
+n(neutrum, singularis, indefinit, genitiv) --> [ skals ].
+n(neutrum, singularis, definit, genitiv) --> [ skalets ].
+n(neutrum, pluralis, definit, genitiv) --> [ skalens ].
 
-n(neutrum, singularis) --> [ skal ].
-n(neutrum, singularis) --> [ skalet ].
-n(neutrum, pluralis) --> [ skalen ].
-n(neutrum, singularis) --> [ skals ].
-n(neutrum, singularis) --> [ skalets ].
-n(neutrum, pluralis) --> [ skalens ].
+n(neutrum, singularis, indefinit, nominativ) --> [ apple ].
+n(neutrum, singularis, definit, nominativ) --> [ applet ].
+n(neutrum, pluralis, indefinit, nominativ) --> [ applen ].
+n(neutrum, pluralis, definit, nominativ) --> [ applena ].
+n(neutrum, singularis, indefinit, genitiv) --> [ apples ].
+n(neutrum, singularis, definit, genitiv) --> [ applets ].
+n(neutrum, pluralis, indefinit, genitiv) --> [ applens ].
+n(neutrum, pluralis, definit, genitiv) --> [ applenas ].
 
-n(neutrum, singularis) --> [ apple].
-n(neutrum, singularis) --> [ applet ].
-n(neutrum, pluralis) --> [ applen ].
-n(neutrum, pluralis) --> [ applena ].
-n(neutrum, singularis) --> [ apples ].
-n(neutrum, singularis) --> [ applets ].
-n(neutrum, pluralis) --> [ applens ].
-n(neutrum, pluralis) --> [ applenas ].
-
-n(utrum, singularis) --> [ kant ].
-n(utrum, singularis) --> [ kanten ].
-n(utrum, pluralis) --> [ kanter ].
-n(utrum, pluralis) --> [ kanterna ].
-n(utrum, singularis) --> [ kants ].
-n(utrum, singularis) --> [ kantens ].
-n(utrum, pluralis) --> [ kanters ].
-n(utrum, pluralis) --> [ kanternas ].
+n(utrum, singularis, indefinit, nominativ) --> [ kant ].
+n(utrum, singularis, definit, nominativ) --> [ kanten ].
+n(utrum, pluralis, indefinit, nominativ) --> [ kanter ].
+n(utrum, pluralis, definit, nominativ) --> [ kanterna ].
+n(utrum, singularis, indefinit, genitiv) --> [ kants ].
+n(utrum, singularis, definit, genitiv) --> [ kantens ].
+n(utrum, pluralis, definit, genitiv) --> [ kanters ].
+n(utrum, pluralis, definit, genitiv) --> [ kanternas ].
 
